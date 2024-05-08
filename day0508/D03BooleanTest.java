9c9a944f1d85673ee740343bf52c392fd2aba527
whdudgms <whdudgms123@naver.com>
2024-05-08 10:55:07 +0900

    init
    init

---
+++ day0508/D03BooleanTest.java
@@ -0,0 +1,21 @@
+class  D03BooleanTest
+{
+	public static void main(String[] args) 
+	{
+		boolean isMan;
+		isMan = true;
+
+
+		String name;
+		name = "홍석영";
+
+		int age;
+		age = 20;
+
+		System.out.println("name : " + name);
+		System.out.println("age : " + age);
+		System.out.println("isMan : " + isMan);
+
+	
+	}
+}
